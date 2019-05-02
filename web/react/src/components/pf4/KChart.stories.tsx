a29ae0064c336106b432ac6334d3fca7cd991d11
Joel Takvorian <jtakvori@redhat.com>
2019-05-02 15:22:29 +0200

    Add more tests (issue #5)

--- web/react/src/components/pf4/KChart.stories.tsx
+++ web/react/src/components/pf4/KChart.stories.tsx
@@ -1,43 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import KChart from './KChart';
-import { TimeSeries } from '../../types/Metrics';
 import { getDataSupplier } from '../../utils/victoryChartsUtils';
+import { empty, generateRandomMetricChart } from '../../types/__mocks__/Charts.mock';
 
 import '@patternfly/react-core/dist/styles/base.css';
-import { ChartModel } from '../../types/Dashboards';
 
-const genSeries = (names: string[]): TimeSeries[] => {
-  const t0 = 1556269000;
-  return names.map(name => {
-    const values: [number, number][] = [];
-    for (let i = 0; i < 10; i++) {
-      const x = t0 + 10 * i;
-      const y = Math.floor(Math.random() * 50);
-      values.push([x, y]);
-    }
-    return {
-      values: values,
-      labelSet: { lbl: name }
-    };
-  });
-}
+const withData = generateRandomMetricChart(['a', 'b', 'c']);
 
-const empty: ChartModel = {
-  name: 'Test empty',
-  unit: 'bytes',
-  spans: 6,
-  metric: []
-};
-
-const withData: ChartModel = {
-  name: 'Test with data',
-  unit: 'bytes',
-  spans: 6,
-  metric: genSeries(['a', 'b', 'c'])
-};
-
-storiesOf('PF3 KChart', module)
+storiesOf('PF4 KChart', module)
   .add('empty', () => (
     <KChart
       chart={empty}
